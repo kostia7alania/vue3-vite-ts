4af0432d422b3f41491e50e645b9b9b80779fe9b
Konstantin Bazrov <jimmy34694@gmail.com>
2021-12-20 21:12:03 +0300

    feat: smooth scrool to top after set the route

--- src/router/router.ts
+++ src/router/router.ts
@@ -7,6 +7,11 @@ const history = createWebHistory();
 const router = createRouter({
   history,
   routes,
+  scrollBehavior: function (to, _from, savedPosition) {
+    if (savedPosition) return savedPosition;
+    if (to.hash) return { el: to.hash, behavior: 'smooth' };
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  },
 });
 
 export { router };
